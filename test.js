532af1e1484beff8016e6577864c7c5562ab6839
Ananya-n1 <aannu7198@gmail.com>
2024-07-19 16:03:02 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,22 @@
+// console.log("Ananya")
+// let S1=33;
+// let S2=String(S1);
+// console.log(S2);
+// let value=3;
+// let neg=-value;
+// console.log(neg);
+// const c1= new String("Rishiv");
+// console.log(c1[0]);
+// console.log(c1.__proto__);
+// console.log(c1.length);
+// let mydate=new Date();
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString("default",{
+//     weekday:"long",
+// }));
+// console.log(typeof mydate);
+function f1(n1,n2){
+    console.log(n1+n2)
+}
+f1(2,3)
